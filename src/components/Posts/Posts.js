d02d7f26a753c734aba0f10659ebdba15c55815f
Renato Zanella <renatozr07@gmail.com>
2022-11-11 00:01:24 -0300

    feat: post card

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post';
 import { useSelector } from 'react-redux';
-
 
 import useStyles from './styles';
 
@@ -12,11 +12,15 @@ const Posts = () => {
     console.log(posts);
 
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
