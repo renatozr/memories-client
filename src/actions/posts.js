bb4462b8a17521eacf96cc4073fbdcfdd441d2ab
Renato Zanella <renatozr07@gmail.com>
2022-11-11 18:25:25 -0300

    feat: update post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -10,11 +10,21 @@ export const getPosts = () => async (dispatch) => {
     }
 }
 
-export const createPost = (newPost) => async (dispatch) => {
+export const createPost = (post) => async (dispatch) => {
     try {
-        const { data } = await api.createPost(newPost);
+        const { data } = await api.createPost(post);
 
         dispatch({ type: 'CREATE', payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
+
+        dispatch({ type: 'UPDATE', payload: data });
     } catch (error) {
         console.log(error.message);
     }
