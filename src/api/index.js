fdb1729d34eb4fe576b28a66698be3457a17d319
Renato Zanella <renatozr07@gmail.com>
2022-11-12 02:00:38 -0300

    fix: api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-server-zvyb.onrender.com/posts';
+const url = 'https://memories-api-wxll.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 
